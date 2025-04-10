4e5aac461e8cc755d7493717fe3ebca276443f5d
Cefroad <149056515+Cefroad@users.noreply.github.com>
2025-04-10 19:28:21 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
